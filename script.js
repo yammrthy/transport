c30c06d661acd44e29333e1271304af43dd7caf0
yammrthy <yam.mrthy@gmail.com>
2025-04-22 10:16:01 +0530

    Delete script.js

--- script.js
+++
@@ -1,3 +0,0 @@
-function openExcel() {
-    window.open("https://jsw-my.sharepoint.com/:x:/g/personal/yamuna_m_jsw_in/Ed7jIcfKyUpPn27iPoIK9RMBDZvXd5J7xfeFO-Uo3mCfbw?e=WQv4fk"); // Replace with your OneDrive Excel link
-}
